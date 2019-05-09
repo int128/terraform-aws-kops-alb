welcome18/kops-terraform-starter

Refactor as Terraform module

outputs.tf
@@ -1,0 +1,19 @@
+output "kubernetes_cluster_name" {
+  description = "Kubernetes cluster name"
+  value       = "${var.kubernetes_cluster_name}"
+}
+
+output "sgid_allow_from_nodes" {
+  description = "ID of security group which is allowed from Kubernetes nodes"
+  value       = "${aws_security_group.allow_from_k8s_nodes.id}"
+}
+
+output "kops_vpc_id" {
+  description = "ID of VPC managed by kops"
+  value       = "${data.aws_vpc.kops_vpc.id}"
+}
+
+output "kops_subnet_ids" {
+  description = "IDs of subnets managed by kops"
+  value       = "${data.aws_subnet_ids.kops_subnets.ids}"
+}

variables.tf
@@ -1,0 +1,41 @@
+variable "kubernetes_cluster_name" {
+  type        = "string"
+  description = "Kubernetes cluster name"
+}
+
+variable "kubernetes_ingress_domain" {
+  type        = "string"
+  description = "Domain name for the external/internal ALB"
+}
+
+variable "kubernetes_ingress_port" {
+  type        = "string"
+  description = "Node port of the ingress controller"
+  default     = 30080
+}
+
+variable "kops_ig_nodes_names" {
+  type        = "list"
+  description = "List of name of instance-group nodes managed by kops"
+  default     = ["nodes"]
+}
+
+variable "alb_external_allow_ip" {
+  type        = "list"
+  description = "List of IP addresses to allow to the external ALB"
+
+  default = [
+    "0.0.0.0/0", # all
+  ]
+}
+
+variable "alb_internal_enabled" {
+  type        = "string"
+  description = "Enable the internal ALB (needed if the external ALB is not public)"
+  default     = false
+}
+
+locals {
+  # Hash of kubernetes_cluster_name and kubernetes_ingress_domain
+  cluster_name_hash = "${substr(sha256("${var.kubernetes_cluster_name}/${var.kubernetes_ingress_domain}"), 0, 16)}"
+}

int128/terraform-aws-kops-alb

outputs.tf
@@ -1,0 +1,19 @@
+output "kops_cluster_name" {
+  description = "Kubernetes cluster name"
+  value       = "${var.kops_cluster_name}"
+}
+
+output "sgid_allow_from_nodes" {
+  description = "ID of security group which is allowed from Kubernetes nodes"
+  value       = "${aws_security_group.allow_from_k8s_nodes.id}"
+}
+
+output "kops_vpc_id" {
+  description = "ID of VPC managed by kops"
+  value       = "${data.aws_vpc.kops_vpc.id}"
+}
+
+output "kops_subnet_ids" {
+  description = "IDs of subnets managed by kops"
+  value       = "${data.aws_subnet_ids.kops_subnets.ids}"
+}

variables.tf
@@ -1,0 +1,41 @@
+variable "kops_cluster_name" {
+  type        = "string"
+  description = "Kubernetes cluster name"
+}
+
+variable "kops_ig_nodes_names" {
+  type        = "list"
+  description = "List of name of instance-group nodes managed by kops"
+  default     = ["nodes"]
+}
+
+variable "kubernetes_ingress_domain" {
+  type        = "string"
+  description = "Domain name for the external/internal ALB"
+}
+
+variable "kubernetes_ingress_port" {
+  type        = "string"
+  description = "Node port of the ingress controller"
+  default     = 30080
+}
+
+variable "alb_external_allow_ip" {
+  type        = "list"
+  description = "List of IP addresses to allow to the external ALB"
+
+  default = [
+    "0.0.0.0/0", # all
+  ]
+}
+
+variable "alb_internal_enabled" {
+  type        = "string"
+  description = "Enable the internal ALB (needed if the external ALB is not public)"
+  default     = false
+}
+
+locals {
+  # Hash of kops_cluster_name and kubernetes_ingress_domain
+  cluster_name_hash = "${substr(sha256("${var.kops_cluster_name}/${var.kubernetes_ingress_domain}"), 0, 16)}"
+}